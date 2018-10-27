a08abb2ed501d55f5a58349e71b31547abb67cc8
Amr El-Madah <amr.nasser.elmadah@gmail.com>
2018-10-27 20:36:09 +0200

    Create the project

---
+++ app/src/main/java/com/mytaxi/task/VehiclesActivity.kt
@@ -0,0 +1,12 @@
+package com.mytaxi.task
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class VehiclesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vehicles)
+    }
+}
